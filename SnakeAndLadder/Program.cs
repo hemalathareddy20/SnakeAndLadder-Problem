29376201026c17faba08ca96453244421cf035b1
hemalathareddy20 <hemalathareddy401@gmail.com>
2020-12-21 11:01:10 +0530

    [Hemalatha] Add:Implementation of Snake and Lader Game with single player

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        int START_POSITION = 0;
+        int PLAYER = 1;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Sanke and Ladder program");
+        }
+    }
+}
