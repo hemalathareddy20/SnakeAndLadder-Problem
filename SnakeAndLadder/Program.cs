dc211e5dfb972f016c61d26bff5ed2ccddb842b3
hemalathareddy20 <hemalathareddy401@gmail.com>
2020-12-21 11:32:30 +0530

    [Hemalatha] Add:Implementation of Snake and Lader Game Using Random to get number between 1 to 6

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -6,9 +6,18 @@ namespace SnakeAndLadder
     {
         int START_POSITION = 0;
         int PLAYER = 1;
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Random random = new Random();
             Console.WriteLine("Welcome to Sanke and Ladder program");
+            int diceNum = rollingDice();
+            Console.WriteLine(" diceNum: " + diceNum); 
+            int rollingDice()
+            {
+                int diceNum = random.Next(1,7);
+                return diceNum;
+            }
+
         }
     }
 }
